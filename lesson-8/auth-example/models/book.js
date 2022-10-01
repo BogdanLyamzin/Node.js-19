ca82792e1ab9227a6c27a8c3a7ec91f01bd2ae5e
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-10-01 13:21:18 +0300

    finish lesson 8

---
+++ lesson-8/auth-example/models/book.js
@@ -0,0 +1,66 @@
+const {Schema, model} = require("mongoose");
+const Joi = require("joi");
+
+const {handleMongooseSchemaError} = require("../helpers");
+
+const genres = ["fantastic", "love"];
+const isbnRegexp = /^\d{3}-\d-\d{3}-\d{5}-\d$/
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    },
+    genre: {
+        type: String,
+        enum: genres,
+        required: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+        required: true,
+    }
+    // isbn: {
+    //     type: String,
+    //     match: isbnRegexp,
+    //     required: true,
+    //     unique: true,
+    // }
+}, {versionKey: false, timestamps: true});
+
+bookSchema.post("save", handleMongooseSchemaError);
+
+const addSchema = Joi.object({
+    title: Joi.string().required(),
+    author: Joi.string().required(),
+    favorite: Joi.boolean(),
+    genre: Joi.string().valueOf(genres).required(),
+    isbn: Joi.string().pattern(isbnRegexp).required(),
+});
+
+const updateFovirteSchema = Joi.object({
+    favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+    addSchema,
+    updateFovirteSchema,
+}
+
+const Book = model("book", bookSchema);
+// categories => category
+// mice => mouse
+
+module.exports = {
+    Book,
+    schemas,
+};
